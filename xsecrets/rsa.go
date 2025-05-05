21abb874fa44d3b060471744516ce7fbc7e7a828
Первушин <e.pervushin@talenttech.ru>
2025-05-05 20:35:41 +0300

    fix: rsa use sha1 for supporting in ruby

--- xsecrets/rsa.go
+++ xsecrets/rsa.go
@@ -3,13 +3,13 @@ package xsecrets
 import (
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha256"
+	"crypto/sha1"
 	"fmt"
 )
 
 // RSAEncryptBytes encrypts raw []byte with the given RSA public key.
 func RSAEncryptBytes(pub *rsa.PublicKey, plaintext []byte) ([]byte, error) {
-	cipherBytes, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, pub, plaintext, nil)
+	cipherBytes, err := rsa.EncryptOAEP(sha1.New(), rand.Reader, pub, plaintext, nil)
 	if err != nil {
 		return nil, fmt.Errorf("rsa encryption failed: %w", err)
 	}
@@ -18,7 +18,7 @@ func RSAEncryptBytes(pub *rsa.PublicKey, plaintext []byte) ([]byte, error) {
 
 // RSADecryptBytes decrypts raw RSA-encrypted []byte using private key.
 func RSADecryptBytes(ciphertext []byte, priv *rsa.PrivateKey) ([]byte, error) {
-	plain, err :=  rsa.DecryptOAEP(sha256.New(), rand.Reader, priv, ciphertext, nil)
+	plain, err :=  rsa.DecryptOAEP(sha1.New(), rand.Reader, priv, ciphertext, nil)
 	if err != nil {
 		return nil, fmt.Errorf("rsa decryption failed: %w", err)
 	}
